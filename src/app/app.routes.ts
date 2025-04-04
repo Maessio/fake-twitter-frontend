d31d06f32c8e4801de756632fdfd7a519a4a4fb1
Maessio <maessiosousa@gmail.com>
2025-04-04 02:34:41 -0300

    refactor: adding project configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FollowingComponent } from './pages/following/following.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: 'login', component: LoginComponent }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'following', component: FollowingComponent }, 
+  { path: 'profile', component: ProfileComponent }, 
+  { path: '**', redirectTo: 'login' }
+];
