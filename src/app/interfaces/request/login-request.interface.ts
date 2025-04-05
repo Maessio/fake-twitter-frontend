4b347d7df3794f224dc1bd6010551ccf3d161135
Maessio <maessiosousa@gmail.com>
2025-04-05 19:43:07 -0300

    feat: creating change password and logout

--- src/app/interfaces/request/login-request.interface.ts
+++ src/app/interfaces/request/login-request.interface.ts
@@ -1,4 +1,5 @@
 export interface LoginRequest {
+    userId: number;
     email: string;
     password: string;
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,14 +1,18 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FooterComponent } from "../../components/footer/footer.component";
-import { UserProfile } from '../../interfaces/user-profile.interface';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../../services/user.service';
+import { UserProfile } from '../../interfaces/user-profile.interface';
+import { AuthService } from '../../services/auth.service';
+import { FooterComponent } from "../../components/footer/footer.component";
 import { PostComponent } from "../../components/post/post.component";
+import { NgForOf } from '@angular/common';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FooterComponent, PostComponent, CommonModule],
+  imports: [ReactiveFormsModule, FooterComponent, PostComponent, NgForOf, ModalComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
@@ -18,15 +22,29 @@ export class ProfileComponent implements OnInit {
 
   isUserLoged = true;
   userProfile?: UserProfile;
+  
+  modalOpen = false;
+  modalTitle = '';
+  modalMessage = '';
 
   private userService = inject(UserService);
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   private currentUserId = Number(localStorage.getItem('userId'));
 
+  passwordForm!: FormGroup;
 
   ngOnInit(): void {
-    if  (!this.userProfile) {
+    if (!this.userProfile) {
       this.loadProfile(this.userId ?? this.currentUserId);
     }
+
+    this.passwordForm = this.fb.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
   loadProfile(id: number): void {
@@ -36,17 +54,56 @@ export class ProfileComponent implements OnInit {
       },
       error: (error) => {
         console.error('Error to load profile:', error);
+
+        this.showModal('Oops!', 'We had a problem loading your user data. Please log in again.');
+
+        this.router.navigate(['/login']);
       }
     });
   }
 
   changePassword() {
-     // leva para a tela de mudança de senha
+    if (this.passwordForm.invalid) {
+      this.passwordForm.markAllAsTouched();
+      return;
+    }
+
+    const { oldPassword, newPassword } = this.passwordForm.value;
+
+    this.authService.changeUserPassword(this.currentUserId, {
+      oldPassword,
+      newPassword,
+    }).subscribe({
+      next: () => {
+        this.showModal('Success', 'Password changed successfully!');
+
+        (document.getElementById('change_password') as HTMLDialogElement)?.close();
+        this.passwordForm.reset();
+      },
+      error: (error) => {
+        console.log("ERROR: "+ error);
+        console.log('Error body:', error.error);
+        this.showModal('Oops!', 'Please try changing your password again.');
+      }
+    });
   }
 
   logout() {
-    // mostra um modal
-    // limpa os dados na userDataService
-    // encaminha para o login
+    this.authService.logout(this.currentUserId).subscribe({
+      next: () => {
+        localStorage.clear();
+
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.showModal('Oops!', 'Please try to logout again.');
+      }
+    });
+  }
+
+  showModal(title: string, message: string) {
+    this.modalTitle = title;
+    this.modalMessage = message;
+    this.modalOpen = true;
   }
 }
