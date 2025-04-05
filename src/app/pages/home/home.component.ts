2900f8056fd228df2deca49c9c4ea7cc10f2b1d1
Maessio <maessiosousa@gmail.com>
2025-04-05 19:45:05 -0300

    refactor: improving home and login pages interface

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,52 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { Post } from '../../interfaces/post.interface';
-import { PostComponent } from "../../components/post/post.component";
+import { PostService } from '../../services/post.service';
+import { CarouselComponent } from "../../components/carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, CommonModule, PostComponent],
+  imports: [FooterComponent, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  imageList: string[] = ["https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp", "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp", "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"]
-  postList: Post[] = [
-    {
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
-    },
-    { 
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
-    },
-    {
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
-    },
-    {
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
-    },
-    {
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
-    },
-    {
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
-    },
-    {
-      username: 'Jane_doe',
-      userId: 2,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere, officia minima, perferendis iusto iure consectetur laudantium soluta corporis aspernatur exercitationem magnam alias eos? Natus nihil est officia ratione cupiditate'
+export class HomeComponent implements OnInit{
+  
+  postList?: Post[];
+  
+  modalOpen = false;
+  modalTitle = '';
+  modalMessage = '';
+
+  private postService = inject(PostService);
+
+  ngOnInit(): void {
+
+    if  (!this.postList) {
+      this.loadRandomPosts();
     }
-  ]
+  }
+    
+  loadRandomPosts() {
+    this.postService.randomPosts().subscribe({
+      next: (posts) => {
+        this.postList = posts;
+      },
+      error: (error) => {
+        console.error('Error to load random posts:', error);
+      }
+    });
+  }
+
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, ModalComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,25 +17,36 @@ export class LoginComponent {
   private router = inject(Router);
   private authService = inject(AuthService);
 
+  modalOpen = false;
+  modalTitle = '';
+  modalMessage = '';
+
   loginForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required]
   });
 
   onSubmit(): void {
+    localStorage.clear();
+
     if (this.loginForm.invalid) return;
 
     this.authService.login(this.loginForm.value).subscribe({
       next: (res) => {
-        alert(res)
         localStorage.setItem('token', res.token);
         localStorage.setItem('userId', res.userId.toString());
 
         this.router.navigate(['/home']);
       },
       error: (err) => {
-        console.error('Login failed:', err);
+        this.showModal('Invalid Credentials!', 'User email or password are incorret.');
       }
     });
+  }
+
+  showModal(title: string, message: string) {
+    this.modalTitle = title;
+    this.modalMessage = message;
+    this.modalOpen = true;
   }
 }
